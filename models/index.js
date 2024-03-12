aa06bd383c2cc0b01e3adde1b8fc8ff0a3fb0297
Dan Nadler <dan.nadlerp@gmail.com>
2024-03-12 10:35:33 -0400

    filled in models and index.js with relationships

--- models/index.js
+++ models/index.js
@@ -1,16 +1,15 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: "category_id" });
+Category.hasMany(Product, { foreignKey: "category_id" });
 
-// Categories have many Products
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" });
 
 module.exports = {
   Product,
