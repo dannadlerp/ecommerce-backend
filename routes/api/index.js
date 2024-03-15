2705d090ab7d5bb7dc8e1bde6c974367759b7629
Dan Nadler <dan.nadlerp@gmail.com>
2024-03-15 12:32:55 -0400

    updated producttable routing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require("express").Router();
+const categoryRoutes = require("./category-routes");
+const productRoutes = require("./product-routes");
+const tagRoutes = require("./tag-routes");
+const productTagRoutes = require("./product-tag-routes");
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/tags", tagRoutes);
+router.use("/product-tags", productTagRoutes);
 
 module.exports = router;
